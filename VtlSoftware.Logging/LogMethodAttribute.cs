cd564883c2ee625d4befa47461542931223e0fcc
Dom Sinclair <dom@viewtolearn.co.uk>
2023-05-25 12:34:25 +0100

    Add project files.

---
+++ VtlSoftware.Logging/LogMethodAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Metalama.Extensions.DependencyInjection;
+using Metalama.Framework.Aspects;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace VtlSoftware.Logging
+{
+    public class LogMethodAttribute : OverrideMethodAspect
+    {
+        #region Fields
+
+        [IntroduceDependency]
+        private readonly ILogger? logger;
+
+        #endregion
+
+        #region Public Methods
+
+        public override dynamic? OverrideMethod()
+        {
+            try
+            {
+                var result = meta.Proceed();
+                return result;
+            } catch(Exception ex)
+            {
+                throw;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+This is a solution designed to test out string handeler and Metalama logging.
+
+The purpose behind this is to find an acceptable  way to utilise Microsoft.Extensions.Logging, injecting it via DI and producing an aspect that can then , in use be made available to both text based and structured logging frameworks.
+
+This takes as a staring point information gleaned from the following blog posts.
+
+[Structured Logging and Interpolated Strings in C# 10](https://habr.com/en/articles/591171/)
+
+[Improving logging performance with source generators](https://andrewlock.net/exploring-dotnet-6-part-8-improving-logging-performance-with-source-generators/)
+
+
+
